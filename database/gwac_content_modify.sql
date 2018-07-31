8f50d91549967cf149596be82bfd424c5365ab9d
msw <msw@msw-PC>
2018-08-01 07:26:11 +0800

    功能修改：修改下载页面内容。

---
+++ database/gwac_content_modify.sql
@@ -0,0 +1,17 @@
+INSERT INTO `gwac_content`.`cms_model_field` (`f_modefiel_id`, `f_model_id`, `f_type`, `f_inner_type`, `f_label`, `f_name`, `f_prompt`, `f_def_value`, `f_is_required`, `f_seq`, `f_is_dbl_column`, `f_is_disabled`) VALUES ('306', '7', '4', '0', '软件类型', 'type', NULL, '国产软件', '0', '3', '1', '0');
+INSERT INTO `gwac_content`.`cms_model_field` (`f_modefiel_id`, `f_model_id`, `f_type`, `f_inner_type`, `f_label`, `f_name`, `f_prompt`, `f_def_value`, `f_is_required`, `f_seq`, `f_is_dbl_column`, `f_is_disabled`) VALUES ('307', '7', '4', '0', '授权方式', 'license', NULL, '免费软件', '0', '4', '1', '0');
+INSERT INTO `gwac_content`.`cms_model_field` (`f_modefiel_id`, `f_model_id`, `f_type`, `f_inner_type`, `f_label`, `f_name`, `f_prompt`, `f_def_value`, `f_is_required`, `f_seq`, `f_is_dbl_column`, `f_is_disabled`) VALUES ('308', '7', '4', '0', '界面语言', 'language', NULL, '简体中文', '0', '5', '1', '0');
+INSERT INTO `gwac_content`.`cms_model_field` (`f_modefiel_id`, `f_model_id`, `f_type`, `f_inner_type`, `f_label`, `f_name`, `f_prompt`, `f_def_value`, `f_is_required`, `f_seq`, `f_is_dbl_column`, `f_is_disabled`) VALUES ('339', '7', '3', '0', '运行环境', 'system', NULL, 'winxp,win7,win8', '0', '7', '0', '0');
+
+
+INSERT INTO `gwac_content`.`cms_model_field_custom` (`f_modefiel_id`, `f_key`, `f_value`) VALUES ('306', 'options', '国产软件\r\n国外软件');
+INSERT INTO `gwac_content`.`cms_model_field_custom` (`f_modefiel_id`, `f_key`, `f_value`) VALUES ('307', 'options', '免费软件\r\n收费软件');
+INSERT INTO `gwac_content`.`cms_model_field_custom` (`f_modefiel_id`, `f_key`, `f_value`) VALUES ('308', 'options', '简体中文\r\n繁体中文\r\n英文');
+INSERT INTO `gwac_content`.`cms_model_field_custom` (`f_modefiel_id`, `f_key`, `f_value`) VALUES ('339', 'options', 'winxp\r\nwin7\r\nwin8\r\nwin2000\r\nwin2003\r\nwin2008\r\nlinux\r\nunix\r\nmac\r\n');
+
+
+
+
+
+
+
